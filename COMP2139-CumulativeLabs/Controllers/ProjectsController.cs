cee775ce5f750b1cf0df9452464e20da42f0055f
J-Oskam <justinoskam@gmail.com>
2024-07-13 22:01:33 -0400

    adding unsaved changes

--- COMP2139-CumulativeLabs/Controllers/ProjectsController.cs
+++ COMP2139-CumulativeLabs/Controllers/ProjectsController.cs
@@ -1,12 +1,20 @@
-﻿using COMP2139_CumulativeLabs.Models;
+﻿using COMP2139_CumulativeLabs.Data;
+using COMP2139_CumulativeLabs.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace COMP2139_CumulativeLabs.Controllers {
     public class ProjectsController : Controller {
+
+        private readonly ApplicationDbContext _context;
+        public ProjectsController(ApplicationDbContext context) {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Index() {
             var projects = new List<Project>() {
-                new Project { ProjectId = 1, Name = "Project 1", Description = "first project"}
+                new Project { ProjectId = 1, Name = "Project 1", Description = "first project"},
+                new Project { ProjectId = 4, Name = "Project 4", Description = "fourth project"}
             };
             return View(projects);
         }
